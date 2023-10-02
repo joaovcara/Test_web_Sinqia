3070eb1ec3c638d4b3827646f22cbdb0d135cfb9
joao.cara <joao.cara@outlook.com.br>
2023-10-02 08:54:39 -0300

    Adicionado Cors API, implementação axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import api from './services/api/api';
+
+const APIEstado = api("Estado");
+
+const update = {
+  Id: 33,
+  Sigla: "NW"
+}
+
+const insert = {
+  Descricao: 'Estado sem sigla'
+}
 
 function App() {
+  useEffect(() => {
+    // APIEstado.get("GetAllEstado").then(result => {
+    //     console.log('result', result)
+    //   });
+
+    //APIEstado.put("UpdateEstado", update);
+
+    APIEstado.post("InsertEstado", insert);
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
