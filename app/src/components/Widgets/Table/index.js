f911a3110f3fa71011271103f3f6407f5e2c2926
joao.cara <joao.cara@outlook.com.br>
2023-10-02 22:02:23 -0300

    Ajustes botões ação tabela

--- app/src/components/Widgets/Table/index.js
+++ app/src/components/Widgets/Table/index.js
@@ -5,21 +5,47 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function Table(props) {
-  let columns = []
   const columnDefault = [
     {
       field: 'actions',
       headerName: 'Ações',
-      width: 135,
+      width: 125,
       renderCell: () => (
         <Grid container style={{ display: "flex", justifyContent: "space-between", textAlign: 'right' }}>
-          <Button sx={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} variant="contained" color="primary" onClick={() => props.funcView()}>
+          <Button sx={{
+            maxWidth: '30px',
+            maxHeight: '30px',
+            minWidth: '30px',
+            minHeight: '30px',
+            background: 'gray',
+            '&:hover': {
+              backgroundColor: '#606060',
+            },
+          }} variant="contained" onClick={() => props.funcView()}>
             <SearchIcon />
           </Button>
-          <Button sx={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} variant="contained" color="primary" onClick={() => props.funcUpdate()}>
+          <Button sx={{
+            maxWidth: '30px',
+            maxHeight: '30px',
+            minWidth: '30px',
+            minHeight: '30px',
+            background: 'orange',
+            '&:hover': {
+              backgroundColor: '#e89200',
+            },
+          }} variant="contained" onClick={() => props.funcUpdate()}>
             <EditIcon />
           </Button>
-          <Button sx={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} variant="contained" color="secondary" onClick={() => props.funcDelete()}>
+          <Button sx={{
+            maxWidth: '30px',
+            maxHeight: '30px',
+            minWidth: '30px',
+            minHeight: '30px',
+            background: 'red',
+            '&:hover': {
+              backgroundColor: '#e00000',
+            },
+          }} variant="contained" onClick={() => props.funcDelete()}>
             <DeleteIcon />
           </Button>
         </Grid>
@@ -27,10 +53,10 @@ export default function Table(props) {
     },
   ];
 
-  columns = [...props.columns, ...columnDefault]
+  const columns = [...props.columns, ...columnDefault]
 
   return (
-    <div style={{ height: '80vh', width: '100%', background:' #fff', borderRadius: 5 }}>
+    <div style={{ height: '80vh', width: '100%', background: ' #fff', borderRadius: 5 }}>
       <DataGrid
         rows={props.data}
         columns={columns}
