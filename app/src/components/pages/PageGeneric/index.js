98c23d8716752e8b364751fc08e23edb43fb41f1
joao.cara <joao.cara@outlook.com.br>
2023-10-04 21:05:16 -0300

    Implementação busca

--- app/src/components/pages/PageGeneric/index.js
+++ app/src/components/pages/PageGeneric/index.js
@@ -2,42 +2,61 @@ import { Button, Grid, TextField, Typography } from '@mui/material'
 import Table from '../../Widgets/Table'
 import AddIcon from '@mui/icons-material/Add';
 import Modal from '../../Widgets/Modal'
+import { useEffect, useState } from 'react';
 
 function PageGeneric(props) {
+  const [dataList, setDataList] = useState([]);
+  const [dataListBackup, setDataListBackup] = useState([]);
+  const [textSearch, setTextSearch] = useState('');
+
+  useEffect(() => {
+    setDataList(props.data);
+    setDataListBackup(props.data);
+  }, [props.data])
+
+  useEffect(() => {
+    const filtered = dataListBackup.filter(x => {
+      return x.nome.toLowerCase().includes(textSearch.toLowerCase())
+        || x.descricao.toLowerCase().includes(textSearch.toLowerCase())
+        || x.localizacao.toLowerCase().includes(textSearch.toLowerCase())
+    });
+    setDataList(filtered);
+  }, [textSearch]);
+
+  const handleInputChange = (e) => {
+    const text = e.target.value;
+    setTextSearch(text);
+  }
+
   return (
     <Grid container paddingX={"5%"} paddingY={"2%"}>
       <Grid container justifyContent="space-between" flexWrap="nowrap">
         <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }} >
           <Typography variant='h6' marginRight="10px">{props.title}</Typography>
-          <Button variant='contained' onClick={() => {props.funcActionUser(); props.setAction("Insert")}}>
+          <Button variant='contained' onClick={() => { props.funcActionUser(); props.setAction("Insert") }}>
             <Typography style={{ textTransform: "none", marginRight: "10px" }} variant='subtitle2' >Adicionar</Typography>
             <AddIcon fontSize='small' />
           </Button>
         </div>
-        <TextField style={{ width: "40%" }} variant='filled' size='small' label="Pesquisar" id="fullWidth" />
+        <TextField onChange={handleInputChange} value={textSearch} style={{ width: "40%" }} variant='filled' size='small' label="Pesquisar" id="fullWidth" />
       </Grid>
       <Grid container marginTop="10px">
         <Table
-          data={props.data}
+          data={dataList}
           columns={props.columns}
           funcOpenClose={props.funcOpenClose}
           funcSetAction={props.setAction}
           funcSetObjPontoTuristicoClicked={props.setObjPontoTuristicoClicked}
         />
       </Grid>
-      {
-        props.openClose &&
+      {props.openClose && (
         <Modal
           title={props.title}
           openClose={props.openClose}
           funcOpenClose={props.funcOpenClose}
-          content={
-            <div>
-              {props.content}
-            </div>
-          }
+          content={<div>{props.content}</div>}
         />
-      }
+      )}
     </Grid>
   );
 }
